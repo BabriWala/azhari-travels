7e8994ccd15bfb7408274e283618492882f82a1c
Muhammad Hanzala <91691179+BabriWala@users.noreply.github.com>
2024-12-12 00:00:26 +0600

    Hero Section Animation Added

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,7 +1,10 @@
+"use client"
 // src/components/Hero.tsx
 import React from "react";
-import Link from "next/link";
+// import Link from "next/link";
+import Link from 'next/link'
 import { Icon } from '@iconify/react';
+import { motion } from 'framer-motion';
 
 interface HeroProps {
     title: string;
@@ -18,6 +21,15 @@ const Hero: React.FC<HeroProps> = ({
     buttonLink,
     backgroundImage = 'https://images.pexels.com/photos/18991579/pexels-photo-18991579/free-photo-of-al-azhar-mosque-in-cairo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 }) => {
+    const slideDownVariant = {
+        hidden: { opacity: 0, y: -100 },
+        visible: (delay = 0) => ({
+            opacity: 1,
+            y: 0,
+            transition: { duration: 1, delay, ease: 'easeInOut' },
+        }),
+    };
+
     return (
         <section
             className="relative flex items-center justify-center h-[100vh] w-full bg-cover bg-center"
@@ -25,10 +37,25 @@ const Hero: React.FC<HeroProps> = ({
         >
             <div className="absolute inset-0 bg-black/60 dark:bg-black/40" aria-hidden="true"></div>
             <div className="relative z-10 flex flex-col items-center text-center px-4 sm:px-6 lg:px-8 text-white dark:text-text-dark max-w-3xl">
-                <h1 className="text-4xl md:text-5xl font-extrabold mb-4">{title}</h1>
-                <p className="text-lg md:text-xl mb-6">{description}</p>
+                <motion.h1
+                    initial="hidden"
+                    animate="visible"
+                    variants={slideDownVariant}
+                    custom={0.2} // No delay for title
+                    className="text-4xl md:text-5xl font-extrabold mb-4">{title}</motion.h1>
+                <motion.p
+                    initial="hidden"
+                    animate="visible"
+                    variants={slideDownVariant}
+                    custom={0.8} // 0.3s delay after the title
+                    className="text-lg md:text-xl mb-6">{description}</motion.p>
+                <motion.div
+                    initial="hidden"
+                    animate="visible"
 
-                <div className="relative group">
+                    variants={slideDownVariant}
+                    custom={1.3} // 0.6s delay after the description
+                    className="relative group">
 
                     <button className="bg-primary hover:bg-gradient-third hover:text-primary text-2xl text-white font-semibold py-2 px-4 rounded transition-all duration-300 flex gap-2 items-center">
                         <Icon icon="icon-park-outline:send" /> {buttonText}
@@ -52,7 +79,7 @@ const Hero: React.FC<HeroProps> = ({
                             </Link>
                         </div>
                     </div>
-                </div>
+                </motion.div>
             </div>
         </section>
     );
