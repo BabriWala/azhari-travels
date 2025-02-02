02c80ce363d0203a9375dd43f4c5ffbac765ad89
Muhammad Hanzala <91691179+BabriWala@users.noreply.github.com>
2025-02-03 04:15:11 +0600

    Package Fully Done

--- src/app/package/[slug]/UEContent.tsx
+++ src/app/package/[slug]/UEContent.tsx
@@ -5,6 +5,9 @@ import UEPlace from './UEComponents/UEPlace';
 import UEHotel from './UEComponents/UEHotel';
 import UEAir from './UEComponents/UEAir';
 import UEINI from './UEComponents/UEINI';
+import UELunch from './UEComponents/UELunch';
+import UETravel from './UEComponents/UETravel';
+import UEProcessing from './UEComponents/UEProcessing';
 
 const UEContent = () => {
     // Manage selected pricing plan: "Basic" or "Premium"
@@ -15,26 +18,31 @@ const UEContent = () => {
             <div className="container mx-auto px-4 sm:px-6 lg:px-8">
                 <h2 className="text-3xl md:text-4xl font-bold text-center text-primary  mb-8">
                     ওমরাহ ও মিশর প্যাকেজ
+                    <br />
+                    <span className='text-xl font-normal'>পাশাপাশি ট্যাব গুলো ক্লিক করে অন্যান্য তথ্য দেখুন</span>
                 </h2>
+                {/* Hotel Demo Picture */}
                 {/* Egypt Itinery */}
+
+
                 <UEPrice></UEPrice>
                 <UEPlace></UEPlace>
                 <UEHotel></UEHotel>
+                <UELunch></UELunch>
+                <UETravel></UETravel>
                 <UEAir></UEAir>
                 <UEINI></UEINI>
+                <UEProcessing></UEProcessing>
 
-                {/* Package Includes */}
-                {/* <div className="mb-8">
-                   
-                    <ul className=" gap-1 grid grid-cols-5">
-                        
-                        <li className='bg-white p-5 rounded-lg shadow-sm flex flex-col gap-2'> <div><Icon icon={"material-symbols:circle-outline"}></Icon> </div>সম্পূর্ণ ট্রান্সপোর্ট সেবা ( মিশরে ৫ দিনের জন্য রিজার্ভ বাস থাকবে )</li>
-                        <li className='bg-white p-5 rounded-lg shadow-sm flex flex-col gap-2'> <div><Icon icon={"material-symbols:circle-outline"}></Icon> </div>মক্কা ও মদিনায় দেশীয় খাবার</li>
-                        <li className='bg-white p-5 rounded-lg shadow-sm flex flex-col gap-2'> <div><Icon icon={"material-symbols:circle-outline"}></Icon> </div>মিশরে ২ বেলা দেশীয় খাবার এবং নাস্তা (সকাল ও দুপুর)</li>
-                        <li className='bg-white p-5 rounded-lg shadow-sm flex flex-col gap-2'> <div><Icon icon={"material-symbols:circle-outline"}></Icon> </div>মিশরে ৩ তারকা হোটেল ( ৪ জন এক রুমে)</li>
-                        
-                    </ul>
-                </div> */}
+                <p className=' text-center pt-10 font-bold'>
+                    আল্লাহ আপনার সফরে বরকত দান করুক - আমিন
+                    <br />
+                    আযহারী ট্রাভেলসের সাথেই থাকুন
+
+                </p>
+
+
+
 
 
             </div>
