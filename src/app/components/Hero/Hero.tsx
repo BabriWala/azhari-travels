64d4ba7c8cafd5db89fe5a4f66ac1809f0da0a28
BabriWala <hanzalafarhann@gmail.com>
2026-01-20 13:40:33 +0600

    Build Checking is okay

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,10 +1,8 @@
-"use client"
-// src/components/Hero.tsx
+"use client";
 import React from "react";
-// import Link from "next/link";
-import Link from 'next/link'
-import { Icon } from '@iconify/react';
-import { motion } from 'framer-motion';
+import Link from "next/link";
+import { Icon } from "@iconify/react";
+import { motion, type Variants } from "framer-motion";
 
 interface HeroProps {
     title: string;
@@ -19,14 +17,14 @@ const Hero: React.FC<HeroProps> = ({
     description,
     buttonText,
     buttonLink,
-    backgroundImage = 'https://images.pexels.com/photos/18991579/pexels-photo-18991579/free-photo-of-al-azhar-mosque-in-cairo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    backgroundImage = "https://images.pexels.com/photos/18991579/pexels-photo-18991579/free-photo-of-al-azhar-mosque-in-cairo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 }) => {
-    const slideDownVariant = {
+    const slideDownVariant: Variants = {
         hidden: { opacity: 0, y: -100 },
-        visible: (delay = 0) => ({
+        visible: (custom: number = 0) => ({
             opacity: 1,
             y: 0,
-            transition: { duration: 1, delay, ease: 'easeInOut' },
+            transition: { duration: 1, delay: custom, ease: "easeInOut" },
         }),
     };
 
@@ -35,35 +33,41 @@ const Hero: React.FC<HeroProps> = ({
             className="relative flex items-center justify-center h-[100vh] w-full bg-cover bg-center"
             style={{ backgroundImage: `url(${backgroundImage})` }}
         >
-            <div className="absolute inset-0 bg-black/60 " aria-hidden="true"></div>
-            <div className="relative z-10 flex flex-col items-center text-center px-4 sm:px-6 lg:px-8 text-white  max-w-3xl">
+            <div className="absolute inset-0 bg-black/60" aria-hidden="true"></div>
+
+            <div className="relative z-10 flex flex-col items-center text-center px-4 sm:px-6 lg:px-8 text-white max-w-3xl">
                 <motion.h1
                     initial="hidden"
                     animate="visible"
                     variants={slideDownVariant}
-                    custom={0} // No delay for title
-                    className="text-4xl md:text-5xl font-extrabold mb-4">{title}</motion.h1>
+                    custom={0}
+                    className="text-4xl md:text-5xl font-extrabold mb-4"
+                >
+                    {title}
+                </motion.h1>
+
                 <motion.p
                     initial="hidden"
                     animate="visible"
                     variants={slideDownVariant}
-                    custom={0.6} // 0.3s delay after the title
-                    className="text-lg md:text-xl mb-6">{description}</motion.p>
+                    custom={0.6}
+                    className="text-lg md:text-xl mb-6"
+                >
+                    {description}
+                </motion.p>
+
                 <motion.div
                     initial="hidden"
                     animate="visible"
-
                     variants={slideDownVariant}
-                    custom={1} // 0.6s delay after the description
-                    className="relative group">
-
+                    custom={1}
+                    className="relative group"
+                >
                     <button className="bg-primary hover:bg-gradient-third hover:text-primary text-2xl text-white font-semibold py-2 px-4 rounded transition-all duration-300 flex gap-2 items-center">
                         <Icon icon="icon-park-outline:send" /> {buttonText}
                     </button>
 
-
-                    {/* Pop-up menu for contact options */}
-                    <div className="absolute w-full bottom-full left-1/2 transform -translate-x-1/2 mb-2 bg-gradient-secondary  text-gray-800  shadow-lg rounded-lg p-3 opacity-0 scale-95 transition-all duration-300 group-hover:opacity-100 group-hover:scale-100">
+                    <div className="absolute w-full bottom-full left-1/2 transform -translate-x-1/2 mb-2 bg-gradient-secondary text-gray-800 shadow-lg rounded-lg p-3 opacity-0 scale-95 transition-all duration-300 group-hover:opacity-100 group-hover:scale-100">
                         <div className="flex items-center justify-center gap-5">
                             <Link href="https://facebook.com/iamhamzahkhan" target="_blank" rel="noopener noreferrer">
                                 <Icon icon="mdi:facebook" className="text-primary text-3xl hover:text-blue-600 transition-colors duration-200" />
